a95e5416d23a36b5dfb91fd1f869bd6db7bcefe3
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-03-23 17:26:43 +0530

    Updates

---
+++ precache-manifest.a14ed68ae14ffb95cf0c3cdf5fefd861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c812df4840a0693c29d4adeb35fc8dca",
+    "url": "/user-info-react/index.html"
+  },
+  {
+    "revision": "cafc6b2c8cbc6f587630",
+    "url": "/user-info-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f978d8111dd5f905faa6",
+    "url": "/user-info-react/static/js/2.8672e460.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/user-info-react/static/js/2.8672e460.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cafc6b2c8cbc6f587630",
+    "url": "/user-info-react/static/js/main.646d545b.chunk.js"
+  },
+  {
+    "revision": "9e6b04a77f0205071a7e",
+    "url": "/user-info-react/static/js/runtime-main.133b4161.js"
+  }
+]);
